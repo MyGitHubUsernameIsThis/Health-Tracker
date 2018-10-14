59ffa57cabc28789756503485e02696af57b7d77
Oliver Rodas <oar5@duke.edu>
2018-10-14 13:02:57 -0400

    Update Installer.py
    Installer necessary to use NutriFact.py

---
+++ Installer.py
@@ -0,0 +1,6 @@
+import os
+os.system("sudo easy_install pip\n"
+          "pip install clarifai\n"
+          "pip install matplotlib\n")
+
+print("Successfully installed the necessary programs to use NutriFact.py")
