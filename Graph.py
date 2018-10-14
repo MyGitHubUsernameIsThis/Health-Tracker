724e46ae7a44b398bf5264730f0dba0a60dd827d
low10high10 <42759455+low10high10@users.noreply.github.com>
2018-10-14 13:10:11 -0400

    Add files via upload
    Graph Module

---
+++ Graph.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def read(fileName):
+    f = open(fileName)
+    caloriesPerDay = {}
+    for line in f:
+        data = line.split(":")
+        day = int(data[0])
+        calories = float(data[1])
+        if day not in caloriesPerDay.keys():
+            caloriesPerDay[day] = 0
+        caloriesPerDay[day] += calories
+    return(caloriesPerDay)
+
+def graph(d):
+    days = []
+    calories = []
+    for k,v in d.items():
+        days.append(k)
+        calories.append(v)
+    newDays = [days[x] - days[0] + 1 for x in range(len(days))]
+    plt.scatter(newDays, calories)
+    plt.title("Calories per Day")
+    plt.plot(newDays, calories)
+    plt.axis([0, len(days) + 1, 0, max(calories) + 100])
+    plt.show()
+
+
