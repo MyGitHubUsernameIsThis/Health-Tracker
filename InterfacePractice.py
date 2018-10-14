4fff601479bdfbc66c506763ba461e8846ef12e6
MyGitHubUsernameIsThis <42785427+MyGitHubUsernameIsThis@users.noreply.github.com>
2018-10-14 12:58:05 -0400

    Delete InterfacePractice.py

--- InterfacePractice.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-from tkinter import filedialog
-
-root = tkinter.Tk()
-file = tkinter.filedialog.askopenfile(parent=root,mode='rb',title='Choose a file')
-if file != None:
-    data = file.read()
-    file.close()
-    print ("It worked...?" )
